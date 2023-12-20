9dadeaf317da4c5f01cd27641f086a9c072c6d34
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2023-12-20 22:54:05 +0100

    Improve error stack driver.

--- src/applicative/error.c
+++ src/applicative/error.c
@@ -38,35 +38,34 @@ void ERROR_stack_init(void) {
 
 /*******************************************************************/
 void ERROR_stack_add(ERROR_code_t code) {
-	// Add error code.
-	error_ctx.stack[error_ctx.stack_idx] = code;
-	// Increment index.
-	error_ctx.stack_idx = ((error_ctx.stack_idx + 1) % ERROR_STACK_DEPTH);
+	// Check index.
+	if (error_ctx.stack_idx < ERROR_STACK_DEPTH) {
+		// Add error code.
+		error_ctx.stack[error_ctx.stack_idx] = code;
+		error_ctx.stack_idx++;
+	}
 }
 
 /*******************************************************************/
 ERROR_code_t ERROR_stack_read(void) {
-	// Read last error.
-	error_ctx.stack_idx = (error_ctx.stack_idx == 0) ? (ERROR_STACK_DEPTH - 1) : (error_ctx.stack_idx - 1);
-	ERROR_code_t last_error = error_ctx.stack[error_ctx.stack_idx];
-	// Remove error.
-	error_ctx.stack[error_ctx.stack_idx] = SUCCESS;
-	// Return code.
+	// Local variables.
+	ERROR_code_t last_error = SUCCESS;
+	// Check index.
+	if (error_ctx.stack_idx > 0) {
+		// Read last error.
+		error_ctx.stack_idx--;
+		last_error = error_ctx.stack[error_ctx.stack_idx];
+		// Remove error.
+		error_ctx.stack[error_ctx.stack_idx] = SUCCESS;
+	}
 	return last_error;
 }
 
 /*******************************************************************/
 uint8_t ERROR_stack_is_empty(void) {
 	// Local variables.
-	uint8_t is_empty = 1;
-	uint8_t idx = 0;
-	// Loop on stack.
-	for (idx=0 ; idx<ERROR_STACK_DEPTH ; idx++) {
-		if (error_ctx.stack[idx] != SUCCESS) {
-			is_empty = 0;
-			break;
-		}
-	}
+	uint8_t is_empty = (error_ctx.stack_idx == 0) ? 1 : 0;
+	// Return flag.
 	return is_empty;
 }
 
