5c3e3afca35d62e6894f91dffb577174fe7215ce
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2025-01-25 19:04:09 +0100

    Add STM32L0xx device submodule.

---
+++ drivers/device/inc/stm32l0xx_device_flags.h
@@ -0,0 +1,16 @@
+/*
+ * stm32l0xx_device_flags.h
+ *
+ *  Created on: 19 jan. 2025
+ *      Author: Ludo
+ */
+
+#ifndef __STM32L0XX_DEVICE_FLAGS_H__
+#define __STM32L0XX_DEVICE_FLAGS_H__
+
+/*** STM32L0XX device compilation flags ***/
+
+#define STM32L0XX_DEVICE_STACK_SIZE     0x00000400
+#define STM32L0XX_DEVICE_HEAP_SIZE      0x00000C00
+
+#endif /* __STM32L0XX_DEVICE_FLAGS_H__ */
